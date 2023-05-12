8c4408402fef43648cc7513d8a1ed96aa1cda256
Ian Neo <ian.neo@checkbox.ai>
2023-05-12 10:24:36 +0800

    [DEV-10882] Updated the naming of the resolver props

--- blocks/sharepoint/sharepoint.ts
+++ blocks/sharepoint/sharepoint.ts
@@ -117,14 +117,14 @@ export class Sharepoint {
                         label: "Select Folder",
                         placeholder: "Select folder",
                         isSearchable: true,
-                        options: async (cbk, customSearchProps) => {
+                        options: async (cbk, customResolverProps) => {
                             const response = await cbk.api.get<
                                 SharepointData[]
                             >("/public/integrations/sharepoint/folders", {
                                 siteId: cbk.getElementValue("site_id"),
                                 driveId: cbk.getElementValue("drive_id"),
-                                searchTerm: customSearchProps?.searchTerm,
-                                itemId: customSearchProps?.selectedValue,
+                                searchTerm: customResolverProps?.searchTerm,
+                                itemId: customResolverProps?.selectedValue,
                             });
 
                             if (!response) return [];
